1e303144ab9cbad30b42e85c85cde557949537b8
Mayur Dilip Tawade <87850085+Mayuralf2001@users.noreply.github.com>
2022-08-02 00:27:12 +0530

    Add files via upload

---
+++ Programs/Comments.cpp
@@ -0,0 +1,12 @@
+/*
+Name: Mayur                 Multi-Line Comments
+Program: Comments in C++
+*/
+#include <iostream>                             //Library/header inclusion
+using namespace std;                            //Name space
+
+int main()                                      //Execution of program begins here have int as return type
+{
+    cout<<"This is comments program"<<endl;     //Pushing output to console
+    return 0;                                   //Value must be returned at the end of main()
+}
